61e1b2de8ecc3e50200065ec97e5a45aa92702b3
Donne Martin <donne.martin@gmail.com>
2015-06-30 07:31:34 -0400

    Added palindrome challenge.

---
+++ linked_lists/palindrome/test_palindrome.py
@@ -0,0 +1,44 @@
+from nose.tools import assert_equal
+
+
+class TestPalindrome(object):
+    
+    def test_palindrome(self):
+        print('Test: Empty list')
+        linked_list = MyLinkedList()
+        assert_equal(linked_list.is_palindrome(), False)
+
+        print('Test: Single element list')
+        head = Node(1)
+        linked_list = MyLinkedList(head)
+        assert_equal(linked_list.is_palindrome(), False)
+
+        print('Test: Two element list, not a palindrome')
+        linked_list.append(2)
+        assert_equal(linked_list.is_palindrome(), False)
+
+        print('Test: General case: Palindrome with even length')
+        head = Node('a')
+        linked_list = MyLinkedList(head)
+        linked_list.append('b')
+        linked_list.append('b')
+        linked_list.append('a')
+        assert_equal(linked_list.is_palindrome(), True)
+
+        print('Test: General case: Palindrome with odd length')
+        head = Node(1)
+        linked_list = MyLinkedList(head)
+        linked_list.append(2)
+        linked_list.append(3)
+        linked_list.append(2)
+        linked_list.append(1)
+        assert_equal(linked_list.is_palindrome(), True)
+        
+        print('Success: test_palindrome')
+
+def main():
+    test = TestPalindrome()
+    test.test_palindrome()
+
+if __name__ == '__main__':
+    main()
