0b5acdb52ffc329db238bfa293cf0954695f0f85
Donne Martin <donne.martin@gmail.com>
2015-07-01 06:48:04 -0400

    Added fibonacci challenge.

---
+++ recursion_dynamic/fibonacci/test_fibonacci.py
@@ -0,0 +1,21 @@
+from nose.tools import assert_equal
+
+
+class TestFib(object):
+    
+    def test_fib(self, func):
+        result = []
+        for i in xrange(num_items):
+            result.append(func(i))
+        fib_seq = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+        assert_equal(result, fib_seq)
+        print('Success: test_fib')
+
+def main():
+    test = TestFib()
+    test.test_fib(fib_recursive)
+    test.test_fib(fib_dynamic)
+    test.test_fib(fib_iterative)
+
+if __name__ == '__main__':
+    main()
