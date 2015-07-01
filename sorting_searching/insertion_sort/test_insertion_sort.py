4a0a2c1267be996bebc103a50116f5aaf199c410
Donne Martin <donne.martin@gmail.com>
2015-07-01 06:47:22 -0400

    Added insertion sort challenge.

---
+++ sorting_searching/insertion_sort/test_insertion_sort.py
@@ -0,0 +1,26 @@
+from nose.tools import assert_equal
+
+
+class TestInsertionSort(object):
+    
+    def test_insertion_sort(self):
+        print('Empty input')
+        data = []
+        insertion_sort(data)
+        assert_equal(data, [])
+
+        print('One element')
+        data = [5]
+        insertion_sort(data)
+        assert_equal(data, [5])
+
+        print('Two or more elements')
+        data = [5, 1, 7, 2, 6, -3, 5, 7, -1]
+        insertion_sort(data)
+        assert_equal(data, sorted(data))
+        
+        print('Success: test_insertion_sort')
+
+if __name__ == '__main__':
+    test = TestInsertionSort()
+    test.test_insertion_sort()
