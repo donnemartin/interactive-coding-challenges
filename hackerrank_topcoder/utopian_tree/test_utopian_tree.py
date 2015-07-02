0286ca37352e1afa82d10b90cdc426daabb06a4d
Donne Martin <donne.martin@gmail.com>
2015-07-02 05:57:29 -0400

    Added utopian tree challenge.

---
+++ hackerrank_topcoder/utopian_tree/test_utopian_tree.py
@@ -0,0 +1,17 @@
+from nose.tools import assert_equal
+
+
+class TestUtopianTree(object):
+
+    def test_utopian_tree(self):
+        assert_equal(calc_utopian_tree_height(0), 1)
+        assert_equal(calc_utopian_tree_height(1), 2)
+        assert_equal(calc_utopian_tree_height(4), 7)
+        print('Success: test_utopian_tree')
+
+def main():
+    test = TestUtopianTree()
+    test.test_utopian_tree()
+
+if __name__ == '__main__':
+    main()
