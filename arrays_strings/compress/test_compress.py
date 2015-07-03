8b9a1473ea27dda3ebccea71464f5f7e9b4c2eae
Donne Martin <donne.martin@gmail.com>
2015-07-03 19:55:56 -0400

    Added compress challenge.

---
+++ arrays_strings/compress/test_compress.py
@@ -0,0 +1,24 @@
+from nose.tools import assert_equal
+
+
+class TestCompress(object):
+    
+    def test_compress(self, func):
+        assert_equal(func(None), None)
+        assert_equal(func(''), '')
+        assert_equal(func('AABBCC'), 'AABBCC')
+        assert_equal(func('AAABCCDDDD'), 'A3B1C2D4')
+        print('Success: test_compress')
+
+def main():
+    test = TestCompress()
+    test.test_compress(compress_string)
+    try:
+        test.test_compress(compress_string_alt)
+    except NameError:
+        # Alternate solutions are only defined
+        # in the solutions file
+        pass
+
+if __name__ == '__main__':
+    main()
