d253a4929aa9e45b74d318ff56137d26a0d62625
Donne Martin <donne.martin@gmail.com>
2015-07-02 23:11:54 -0400

    Added remove duplicates challenge.

---
+++ linked_lists/remove_duplicates/test_remove_duplicates.py
@@ -0,0 +1,36 @@
+from nose.tools import assert_equal
+
+
+class TestRemoveDupes(object):
+    
+    def test_remove_dupes(self, linked_list):
+        print('Test: Empty list')
+        linked_list.remove_dupes()
+        assert_equal(linked_list.get_all_data(), [])
+
+        print('Test: One element list')
+        linked_list.insert_to_front(2)
+        linked_list.remove_dupes()
+        assert_equal(linked_list.get_all_data(), [2])
+
+        print('Test: General case, duplicates')
+        linked_list.insert_to_front(1)
+        linked_list.insert_to_front(3)
+        linked_list.insert_to_front(1)
+        linked_list.insert_to_front(1)
+        linked_list.remove_dupes()
+        assert_equal(linked_list.get_all_data(), [1, 3, 2])
+
+        print('Test: General case, no duplicates')
+        linked_list.remove_dupes()
+        assert_equal(linked_list.get_all_data(), [1, 3, 2])
+        
+        print('Success: test_remove_dupes\n')
+
+def main():
+    test = TestRemoveDupes()
+    linked_list = MyLinkedList(None)
+    test.test_remove_dupes(linked_list)
+    
+if __name__ == '__main__':
+    main()
