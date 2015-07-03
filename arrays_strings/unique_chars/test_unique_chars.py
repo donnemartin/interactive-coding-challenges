8ac0bd3f69fae9d1338f296b9de6f17a5b7a2b7d
Donne Martin <donne.martin@gmail.com>
2015-07-03 19:57:24 -0400

    Added unique chars challenge.

---
+++ arrays_strings/unique_chars/test_unique_chars.py
@@ -0,0 +1,24 @@
+from nose.tools import assert_equal
+
+
+class TestUniqueChars(object):
+
+  def test_unique_chars(self, func):
+        assert_equal(func(''), True)
+        assert_equal(func('foo'), False)
+        assert_equal(func('bar'), True)
+        print('Success: test_unique_chars')
+
+def main():
+    test = TestUniqueChars()
+    test.test_unique_chars(unique_chars)
+    try:
+        test.test_unique_chars(unique_chars_hash)
+        test.test_unique_chars(unique_chars_inplace)
+    except NameError:
+        # Alternate solutions are only defined
+        # in the solutions file
+        pass
+    
+if __name__ == '__main__':
+    main()
