fb7de503af64051ed9c4b7afc0bcc267a1e20566
Donne Martin <donne.martin@gmail.com>
2015-07-03 19:56:38 -0400

    Added permutation challenge.

---
+++ arrays_strings/permutation/test_permutation_solution.py
@@ -0,0 +1,24 @@
+from nose.tools import assert_equal
+
+
+class TestPermutation(object):
+    
+    def test_permutation(self, func):
+        assert_equal(func('', 'foo'), False)
+        assert_equal(func('Nib', 'bin'), False)
+        assert_equal(func('act', 'cat'), True)
+        assert_equal(func('a ct', 'ca t'), True)
+        print('Success: test_permutation')
+
+def main():
+    test = TestPermutation()
+    test.test_permutation(permutations)
+    try:
+        test.test_permutation(permutations_alt)
+    except NameError:
+        # Alternate solutions are only defined
+        # in the solutions file
+        pass
+    
+if __name__ == '__main__':
+    main()
