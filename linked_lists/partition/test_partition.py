c16f25e5f34c2aab7f0b0ed4001b16ef06bf5db3
Donne Martin <donne.martin@gmail.com>
2015-07-02 23:11:35 -0400

    Added partition challenge.

---
+++ linked_lists/partition/test_partition.py
@@ -0,0 +1,45 @@
+from nose.tools import assert_equal
+
+
+class TestPartition(object):
+    
+    def test_partition(self):
+        print('Test: Empty list')
+        linked_list = MyLinkedList(None)
+        linked_list.partition(10)
+        assert_equal(linked_list.get_all_data(), [])
+
+        print('Test: One element list, left list empty')
+        linked_list = MyLinkedList(Node(5))
+        linked_list.partition(0)
+        assert_equal(linked_list.get_all_data(), [5])
+
+        print('Test: Right list is empty')
+        linked_list = MyLinkedList(Node(5))
+        linked_list.partition(10)
+        assert_equal(linked_list.get_all_data(), [5])
+
+        print('Test: General case')
+        # Partition = 10
+        # Input: 4, 3, 7, 8, 10, 1, 10, 12
+        # Output: 4, 3, 7, 8, 1, 10, 10, 12
+        linked_list = MyLinkedList(Node(12))
+        linked_list.insert_to_front(10)
+        linked_list.insert_to_front(1)
+        linked_list.insert_to_front(10)
+        linked_list.insert_to_front(8)
+        linked_list.insert_to_front(7)
+        linked_list.insert_to_front(3)
+        linked_list.insert_to_front(4)
+        partitioned_list = linked_list.partition(10)
+        assert_equal(partitioned_list.get_all_data(), 
+                     [4, 3, 7, 8, 1, 10, 10, 12])
+        
+        print('Success: test_partition')
+
+def main():
+    test = TestPartition()
+    test.test_partition()
+    
+if __name__ == '__main__':
+    main()
