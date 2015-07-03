73ec41e42777bee730db7194f89d92eaad8be629
Donne Martin <donne.martin@gmail.com>
2015-07-03 19:57:09 -0400

    Added reverse string challenge.

---
+++ arrays_strings/reverse_string/test_reverse_string.py
@@ -0,0 +1,18 @@
+from nose.tools import assert_equal
+
+
+class TestReverse(object):
+    
+    def test_reverse(self):
+        assert_equal(list_of_chars(None), None)
+        assert_equal(list_of_chars(['']), [''])
+        assert_equal(list_of_chars(['f', 'o', 'o', ' ', 'b', 'a', 'r']), 
+                                    ['r', 'a', 'b', ' ', 'o', 'o', 'f'])
+        print('Success: test_reverse')
+
+def main():
+    test = TestReverse()
+    test.test_reverse()
+    
+if __name__ == '__main__':
+    main()
