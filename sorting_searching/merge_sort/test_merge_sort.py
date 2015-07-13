e78657f4a42d419dfccd32eb72012fd6e447d83c
Justin LeFebvre <jstnlefebvre@gmail.com>
2015-07-13 00:33:07 -0400

    the 'Two or more elements' test doesn't actually test if the list was sorted properly

--- sorting_searching/merge_sort/test_merge_sort.py
+++ sorting_searching/merge_sort/test_merge_sort.py
@@ -5,18 +5,18 @@ class TestMergeSort(object):
     def test_merge_sort(self):
         print('Empty input')
         data = []
-        merge_sort(data)
-        assert_equal(data, [])
+        sorted_data = merge_sort(data)
+        assert_equal(sorted_data, [])
 
         print('One element')
         data = [5]
-        merge_sort(data)
-        assert_equal(data, [5])
+        sorted_data = merge_sort(data)
+        assert_equal(sorted_data, [5])
 
         print('Two or more elements')
         data = [5, 1, 7, 2, 6, -3, 5, 7, -1]
-        data = merge_sort(data)
-        assert_equal(data, sorted(data))
+        sorted_data = merge_sort(data)
+        assert_equal(sorted_data, sorted(data))
 
         print('Success: test_merge_sort')
 
