e8de164d9dd5009de05472ec861661e4db4f96ea
mrb00l34n <rsabb24@gmail.com>
2015-07-22 11:31:38 -0400

    Fixed pull request changes

--- recursion_dynamic/coinchange_ways/test_coinchange_ways.py
+++ recursion_dynamic/coinchange_ways/test_coinchange_ways.py
@@ -4,9 +4,9 @@ from nose.tools import assert_equal
 class Challenge(object):
 
     def test_coinchange_ways(self,solution):
-        assert_equal(solution(0,2,[1,2]), 0)
-        assert_equal(solution(100,3,[1,2,3]), 884)
-        assert_equal(solution(1000,100,range(1,101)), 15658181104580771094597751280645)
+        assert_equal(solution(0, [1, 2]), 0)
+        assert_equal(solution(100, [1, 2, 3]), 884)
+        assert_equal(solution(1000, range(1, 101)), 15658181104580771094597751280645)
         print('Success: test_coinchange_ways')
 
 
