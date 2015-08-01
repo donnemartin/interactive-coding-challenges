757eca26f1e54713201973e6a793a456fffc6272
Donne Martin <donne.martin@gmail.com>
2015-08-01 09:24:19 -0400

    Added unit testing utility to redirect print output to help validate some functions.

---
+++ graphs_trees/utils/captured_output.py
@@ -0,0 +1,14 @@
+import sys
+from contextlib import contextmanager
+from StringIO import StringIO
+
+
+@contextmanager
+def captured_output():
+    new_out, new_err = StringIO(), StringIO()
+    old_out, old_err = sys.stdout, sys.stderr
+    try:
+        sys.stdout, sys.stderr = new_out, new_err
+        yield sys.stdout, sys.stderr
+    finally:
+        sys.stdout, sys.stderr = old_out, old_err
