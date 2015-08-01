332230c3a1a2b0f8d460756e16d584b8e25f4786
Donne Martin <donne.martin@gmail.com>
2015-08-01 17:48:12 -0400

    Simpilfied breadth first search challenge.

--- graphs_trees/tree_bfs/test_bfs.py
+++ graphs_trees/tree_bfs/test_bfs.py
@@ -5,14 +5,14 @@ from nose.tools import assert_equal
 class TestBfs(object):
 
     def test_bfs(self):
-        root = Node(5)
-        root.insert(2)
-        root.insert(8)
-        root.insert(1)
-        root.insert(3)
+        node = Node(5)
+        insert(node, 2)
+        insert(node, 8)
+        insert(node, 1)
+        insert(node, 3)
 
         with captured_output() as (out, err):
-            root.bfs(sys.stdout.write)
+            bfs(node, sys.stdout.write)
             assert_equal(out.getvalue().strip(), '52813')
 
         print('Success: test_bfs')
