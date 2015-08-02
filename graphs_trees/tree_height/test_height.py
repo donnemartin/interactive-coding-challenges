0f55ccf38d12f1f91243360c0693af4f0b73a543
Donne Martin <donne.martin@gmail.com>
2015-08-02 10:53:35 -0400

    Added tree height challenge.

---
+++ graphs_trees/tree_height/test_height.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+from nose.tools import assert_equal
+
+
+class TestHeight(object):
+
+    def test_height(self):
+        root = Node(5)
+        assert_equal(height(root), 1)
+        insert(root, 2)
+        insert(root, 8)
+        insert(root, 1)
+        insert(root, 3)
+        assert_equal(height(root), 3)
+
+        print('Success: test_height')
+
+
+def main():
+    test = TestHeight()
+    test.test_height()
+
+
+if __name__ == '__main__':
+    main()
