74d25491cae0afb1d4f03961570e5aefbccc5257
Donne Martin <donne.martin@gmail.com>
2015-08-05 05:47:02 -0400

    Tweaked ordering of graph and bst challenges.

---
+++ graphs_trees/graph_path_exists/test_path_exists.py
@@ -0,0 +1,33 @@
+from nose.tools import assert_equal
+
+
+class TestPathExists(object):
+
+    def test_path_exists(self):
+        nodes = []
+        graph = Graph()
+        for id in range(0, 6):
+            nodes.append(graph.add_node(id))
+        graph.add_edge(0, 1, 5)
+        graph.add_edge(0, 4, 3)
+        graph.add_edge(0, 5, 2)
+        graph.add_edge(1, 3, 5)
+        graph.add_edge(1, 4, 4)
+        graph.add_edge(2, 1, 6)
+        graph.add_edge(3, 2, 7)
+        graph.add_edge(3, 4, 8)
+
+        assert_equal(path_exists(nodes[0], nodes[2]), True)
+        assert_equal(path_exists(nodes[0], nodes[0]), True)
+        assert_equal(path_exists(nodes[4], nodes[5]), False)
+
+        print('Success: test_path_exists')
+
+
+def main():
+    test = TestPathExists()
+    test.test_path_exists()
+
+
+if __name__ == '__main__':
+    main()
