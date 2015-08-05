bad87c9f8770254d3005ebdd1632062d6ad90bf1
Donne Martin <donne.martin@gmail.com>
2015-08-04 20:02:28 -0400

    Added graph bfs challenge.

---
+++ graphs_trees/graph_bfs/test_bfs.py
@@ -0,0 +1,34 @@
+from nose.tools import assert_equal
+
+
+class TestBfs(object):
+
+    def __init__(self):
+        self.results = Results()
+
+    def test_bfs(self):
+        nodes = []
+        graph = Graph()
+        for id in range(0, 6):
+            nodes.append(graph.add_node(id))
+        graph.add_edge(0, 1, 5)
+        graph.add_edge(0, 4, 3)
+        graph.add_edge(0, 5, 2)
+        graph.add_edge(1, 3, 5)
+        graph.add_edge(1, 4, 4)
+        graph.add_edge(2, 1, 6)
+        graph.add_edge(3, 2, 7)
+        graph.add_edge(3, 4, 8)
+        bfs(nodes[0], self.results.add_result)
+        assert_equal(str(self.results), "[0, 1, 4, 5, 3, 2]")
+
+        print('Success: test_bfs')
+
+
+def main():
+    test = TestBfs()
+    test.test_bfs()
+
+
+if __name__ == '__main__':
+    main()
