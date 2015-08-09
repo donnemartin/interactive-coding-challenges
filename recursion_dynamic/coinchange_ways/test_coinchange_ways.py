f74d40926c8d83c2bafb809e83343a57877fd44a
Donne Martin <donne.martin@gmail.com>
2015-08-09 07:25:08 -0400

    Renamed coinchange to coin_change.

--- recursion_dynamic/coinchange_ways/test_coinchange_ways.py
+++ recursion_dynamic/coinchange_ways/test_coinchange_ways.py
@@ -3,16 +3,16 @@ from nose.tools import assert_equal
 
 class Challenge(object):
 
-    def test_coinchange_ways(self,solution):
+    def test_coin_change_ways(self,solution):
         assert_equal(solution(0, [1, 2]), 0)
         assert_equal(solution(100, [1, 2, 3]), 884)
         assert_equal(solution(1000, range(1, 101)), 15658181104580771094597751280645)
-        print('Success: test_coinchange_ways')
+        print('Success: test_coin_change_ways')
 
 
 def main():
     test = Challenge()
-    test.test_coinchange_ways(change_ways)
+    test.test_coin_change_ways(change_ways)
 
 
 if __name__ == '__main__':
