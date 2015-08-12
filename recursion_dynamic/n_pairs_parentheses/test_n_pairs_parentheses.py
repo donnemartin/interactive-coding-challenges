c868f891e79c61eeb506f4969960ccea5898ce14
Donne Martin <donne.martin@gmail.com>
2015-08-11 21:13:35 -0400

    Cleaned up n pairs parentheses solution notebook and unit test.

--- recursion_dynamic/n_pairs_parentheses/test_n_pairs_parentheses.py
+++ recursion_dynamic/n_pairs_parentheses/test_n_pairs_parentheses.py
@@ -1,17 +1,25 @@
 from nose.tools import assert_equal
 
+
 class TestPairParentheses(object):
-    
+
     def test_pair_parentheses(self, solution):
         assert_equal(solution(0), set([]))
         assert_equal(solution(1), set(['()']))
-        assert_equal(solution(2), set(['(())', '()()']))
-        assert_equal(solution(3), set(['((()))','(()())', '(())()', '()(())', '()()()']))
+        assert_equal(solution(2), set(['(())', 
+                                       '()()']))
+        assert_equal(solution(3), set(['((()))', 
+                                       '(()())', 
+                                       '(())()', 
+                                       '()(())', 
+                                       '()()()']))
         print('Success: test_pair_parentheses')
+
 
 def main():
     test = TestPairParentheses()
     test.test_pair_parentheses(pair_parentheses)
+
 
 if __name__ == '__main__':
     main()
