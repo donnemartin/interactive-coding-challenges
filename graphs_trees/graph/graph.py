1f45095f6030b159f4f08e75934c76e5572356b9
Donne Martin <donne.martin@gmail.com>
2015-08-23 08:08:46 -0400

    Added three state variable visit_state which will be useful for more advanced challenges such as topological sort.

--- graphs_trees/graph/graph.py
+++ graphs_trees/graph/graph.py
@@ -1,11 +1,18 @@
 from collections import OrderedDict
+from enum import Enum  # Python 2 users: Run pip install enum34
+
+
+class State(Enum):
+    unvisited = 1
+    visited = 2
+    visiting = 3
 
 
 class Node:
 
     def __init__(self, id):
         self.id = id
-        self.visited = False
+        self.visit_state = State.unvisited
         self.adjacent = OrderedDict()  # key = node, val = weight
 
     def __str__(self):
