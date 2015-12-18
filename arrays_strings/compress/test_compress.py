bbb9b74f0f74a89e9d6de2fe4400f1d8f9413fca
Ashhar Hasan <hashhar_dev@outlook.com>
2015-12-19 02:20:59 +0530

    Restored original compression challenge

--- arrays_strings/compress/test_compress.py
+++ arrays_strings/compress/test_compress.py
@@ -7,8 +7,7 @@ class TestCompress(object):
         assert_equal(func(None), None)
         assert_equal(func(''), '')
         assert_equal(func('AABBCC'), 'AABBCC')
-        assert_equal(func('AAABCCDDDD'), 'A3BCCD4')
-        assert_equal(func('aaBCCEFFFFKKMMMMMMP taaammanlaarrrr seeeeeeeeek tooo'), 'aaBCCEF4KKM6P ta3mmanlaar4 se9k to3')
+        assert_equal(func('AAABCCDDDD'), 'A3B1C2D4')
         print('Success: test_compress')
 
 
