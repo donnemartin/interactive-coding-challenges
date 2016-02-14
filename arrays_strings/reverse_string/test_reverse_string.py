21add6f4f5ad4fac700c4541840849cfa31bdc56
Donne Martin <donne.martin@gmail.com>
2016-02-14 06:04:48 -0500

    Rename list_of_chars to reverse_string.

--- arrays_strings/reverse_string/test_reverse_string.py
+++ arrays_strings/reverse_string/test_reverse_string.py
@@ -4,16 +4,16 @@ from nose.tools import assert_equal
 class TestReverse(object):
 
     def test_reverse(self):
-        assert_equal(list_of_chars(None), None)
-        assert_equal(list_of_chars(['']), [''])
-        assert_equal(list_of_chars(
+        assert_equal(reverse_string(None), None)
+        assert_equal(reverse_string(['']), [''])
+        assert_equal(reverse_string(
             ['f', 'o', 'o', ' ', 'b', 'a', 'r']),
             ['r', 'a', 'b', ' ', 'o', 'o', 'f'])
         print('Success: test_reverse')
 
     def test_reverse_inplace(self):
         target_list = ['f', 'o', 'o', ' ', 'b', 'a', 'r']
-        list_of_chars(target_list)
+        reverse_string(target_list)
         assert_equal(target_list, ['r', 'a', 'b', ' ', 'o', 'o', 'f'])
         print('Success: test_reverse_inplace')
 
