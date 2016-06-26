50931033243d4057706f469489e03be9c5099e82
Donne Martin <donne.martin@gmail.com>
2016-06-26 17:48:32 -0400

    Rework graph challenge and solution (#86)

--- graphs_trees/graph/graph.py
+++ graphs_trees/graph/graph.py
@@ -1,11 +1,10 @@
-from collections import OrderedDict
 from enum import Enum  # Python 2 users: Run pip install enum34
 
 
 class State(Enum):
-    unvisited = 1
+    unvisited = 0
+    visiting = 1
     visited = 2
-    visiting = 3
 
 
 class Node:
@@ -13,25 +12,32 @@ class Node:
     def __init__(self, id):
         self.id = id
         self.visit_state = State.unvisited
-        self.adjacent = OrderedDict()  # key = node, val = weight
+        self.adjacent = {}  # key = node, val = weight
 
     def __str__(self):
         return str(self.id)
+
+    def add_neighbor(self, neighbor, weight=0):
+        self.adjacent[neighbor] = weight
 
 
 class Graph:
 
     def __init__(self):
-        self.nodes = OrderedDict()  # key = node id, val = node
+        self.nodes = {}  # key = node id, val = node
 
     def add_node(self, id):
         node = Node(id)
         self.nodes[id] = node
         return node
 
-    def add_edge(self, source, dest, weight=0):
-        if source not in self.nodes:
-            self.add_node(source)
-        if dest not in self.nodes:
-            self.add_node(dest)
-        self.nodes[source].adjacent[self.nodes[dest]] = weight
+    def add_edge(self, id_source, id_dest, weight=0):
+        if id_source not in self.nodes:
+            self.add_node(id_source)
+        if id_dest not in self.nodes:
+            self.add_node(id_dest)
+        self.nodes[id_source].add_neighbor(self.nodes[id_dest], weight)
+
+    def add_undirected_edge(self, source, dest, weight=0):
+        self.add_edge(source, dest, weight)
+        self.nodes[dest].add_neighbor(self.nodes[source], weight)
--- graphs_trees/graph/test_graph.py
+++ graphs_trees/graph/test_graph.py
@@ -3,19 +3,24 @@ from nose.tools import assert_equal
 
 class TestGraph(object):
 
-    def test_graph(self):
+    def create_graph(self):
         graph = Graph()
         for id in range(0, 6):
             graph.add_node(id)
-        graph.add_edge(0, 1, 5)
-        graph.add_edge(0, 5, 2)
-        graph.add_edge(1, 2, 3)
-        graph.add_edge(2, 3, 4)
-        graph.add_edge(3, 4, 5)
-        graph.add_edge(3, 5, 6)
-        graph.add_edge(4, 0, 7)
-        graph.add_edge(5, 4, 8)
-        graph.add_edge(5, 2, 9)
+        return graph
+
+    def test_graph(self):
+        graph = self.create_graph()
+
+        graph.add_edge(0, 1, weight=5)
+        graph.add_edge(0, 5, weight=2)
+        graph.add_edge(1, 2, weight=3)
+        graph.add_edge(2, 3, weight=4)
+        graph.add_edge(3, 4, weight=5)
+        graph.add_edge(3, 5, weight=6)
+        graph.add_edge(4, 0, weight=7)
+        graph.add_edge(5, 4, weight=8)
+        graph.add_edge(5, 2, weight=9)
 
         assert_equal(graph.nodes[0].adjacent[graph.nodes[1]], 5)
         assert_equal(graph.nodes[0].adjacent[graph.nodes[5]], 2)
@@ -29,10 +34,27 @@ class TestGraph(object):
 
         print('Success: test_graph')
 
+    def test_graph_undirected(self):
+        graph = self.create_graph()
+
+        graph.add_undirected_edge(0, 1, weight=5)
+        graph.add_undirected_edge(0, 5, weight=2)
+        graph.add_undirected_edge(1, 2, weight=3)
+
+        assert_equal(graph.nodes[0].adjacent[graph.nodes[1]], 5)
+        assert_equal(graph.nodes[1].adjacent[graph.nodes[0]], 5)
+        assert_equal(graph.nodes[0].adjacent[graph.nodes[5]], 2)
+        assert_equal(graph.nodes[5].adjacent[graph.nodes[0]], 2)
+        assert_equal(graph.nodes[1].adjacent[graph.nodes[2]], 3)
+        assert_equal(graph.nodes[2].adjacent[graph.nodes[1]], 3)
+
+        print('Success: test_graph')
+
 
 def main():
     test = TestGraph()
     test.test_graph()
+    test.test_graph_undirected()
 
 
 if __name__ == '__main__':
