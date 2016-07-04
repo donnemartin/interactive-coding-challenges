8a967e32433eb4382de21775010390e3e18ae8b0
Donne Martin <donne.martin@gmail.com>
2016-07-04 07:15:39 -0400

    Rework graph challenge and solution

--- graphs_trees/graph/test_graph.py
+++ graphs_trees/graph/test_graph.py
@@ -5,13 +5,12 @@ class TestGraph(object):
 
     def create_graph(self):
         graph = Graph()
-        for id in range(0, 6):
-            graph.add_node(id)
+        for key in range(0, 6):
+            graph.add_node(key)
         return graph
 
     def test_graph(self):
         graph = self.create_graph()
-
         graph.add_edge(0, 1, weight=5)
         graph.add_edge(0, 5, weight=2)
         graph.add_edge(1, 2, weight=3)
@@ -22,33 +21,46 @@ class TestGraph(object):
         graph.add_edge(5, 4, weight=8)
         graph.add_edge(5, 2, weight=9)
 
-        assert_equal(graph.nodes[0].adjacent[graph.nodes[1]], 5)
-        assert_equal(graph.nodes[0].adjacent[graph.nodes[5]], 2)
-        assert_equal(graph.nodes[1].adjacent[graph.nodes[2]], 3)
-        assert_equal(graph.nodes[2].adjacent[graph.nodes[3]], 4)
-        assert_equal(graph.nodes[3].adjacent[graph.nodes[4]], 5)
-        assert_equal(graph.nodes[3].adjacent[graph.nodes[5]], 6)
-        assert_equal(graph.nodes[4].adjacent[graph.nodes[0]], 7)
-        assert_equal(graph.nodes[5].adjacent[graph.nodes[4]], 8)
-        assert_equal(graph.nodes[5].adjacent[graph.nodes[2]], 9)
+        assert_equal(graph.nodes[0].adj_weights[graph.nodes[1].key], 5)
+        assert_equal(graph.nodes[0].adj_weights[graph.nodes[5].key], 2)
+        assert_equal(graph.nodes[1].adj_weights[graph.nodes[2].key], 3)
+        assert_equal(graph.nodes[2].adj_weights[graph.nodes[3].key], 4)
+        assert_equal(graph.nodes[3].adj_weights[graph.nodes[4].key], 5)
+        assert_equal(graph.nodes[3].adj_weights[graph.nodes[5].key], 6)
+        assert_equal(graph.nodes[4].adj_weights[graph.nodes[0].key], 7)
+        assert_equal(graph.nodes[5].adj_weights[graph.nodes[4].key], 8)
+        assert_equal(graph.nodes[5].adj_weights[graph.nodes[2].key], 9)
+
+        assert_equal(graph.nodes[0].incoming_edges, 1)
+        assert_equal(graph.nodes[1].incoming_edges, 1)
+        assert_equal(graph.nodes[2].incoming_edges, 2)
+        assert_equal(graph.nodes[3].incoming_edges, 1)
+        assert_equal(graph.nodes[4].incoming_edges, 2)
+        assert_equal(graph.nodes[5].incoming_edges, 2)
+
+        graph.nodes[0].remove_neighbor(graph.nodes[1])
+        assert_equal(graph.nodes[1].incoming_edges, 0)
+        graph.nodes[3].remove_neighbor(graph.nodes[4])
+        assert_equal(graph.nodes[4].incoming_edges, 1)
+
+        assert_equal(graph.nodes[0] < graph.nodes[1], True)
 
         print('Success: test_graph')
 
     def test_graph_undirected(self):
         graph = self.create_graph()
-
         graph.add_undirected_edge(0, 1, weight=5)
         graph.add_undirected_edge(0, 5, weight=2)
         graph.add_undirected_edge(1, 2, weight=3)
 
-        assert_equal(graph.nodes[0].adjacent[graph.nodes[1]], 5)
-        assert_equal(graph.nodes[1].adjacent[graph.nodes[0]], 5)
-        assert_equal(graph.nodes[0].adjacent[graph.nodes[5]], 2)
-        assert_equal(graph.nodes[5].adjacent[graph.nodes[0]], 2)
-        assert_equal(graph.nodes[1].adjacent[graph.nodes[2]], 3)
-        assert_equal(graph.nodes[2].adjacent[graph.nodes[1]], 3)
+        assert_equal(graph.nodes[0].adj_weights[graph.nodes[1].key], 5)
+        assert_equal(graph.nodes[1].adj_weights[graph.nodes[0].key], 5)
+        assert_equal(graph.nodes[0].adj_weights[graph.nodes[5].key], 2)
+        assert_equal(graph.nodes[5].adj_weights[graph.nodes[0].key], 2)
+        assert_equal(graph.nodes[1].adj_weights[graph.nodes[2].key], 3)
+        assert_equal(graph.nodes[2].adj_weights[graph.nodes[1].key], 3)
 
-        print('Success: test_graph')
+        print('Success: test_graph_undirected')
 
 
 def main():
