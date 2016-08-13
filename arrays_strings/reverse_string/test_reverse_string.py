1fde26fcf5d3e58715af13ebab3de2632c358adc
Donne Martin <donne.martin@gmail.com>
2016-08-13 06:40:13 -0400

    Move string reverse to a class

--- arrays_strings/reverse_string/test_reverse_string.py
+++ arrays_strings/reverse_string/test_reverse_string.py
@@ -3,25 +3,26 @@ from nose.tools import assert_equal
 
 class TestReverse(object):
 
-    def test_reverse(self):
-        assert_equal(reverse_string(None), None)
-        assert_equal(reverse_string(['']), [''])
-        assert_equal(reverse_string(
+    def test_reverse(self, func):
+        assert_equal(func(None), None)
+        assert_equal(func(['']), [''])
+        assert_equal(func(
             ['f', 'o', 'o', ' ', 'b', 'a', 'r']),
             ['r', 'a', 'b', ' ', 'o', 'o', 'f'])
         print('Success: test_reverse')
 
-    def test_reverse_inplace(self):
+    def test_reverse_inplace(self, func):
         target_list = ['f', 'o', 'o', ' ', 'b', 'a', 'r']
-        reverse_string(target_list)
+        func(target_list)
         assert_equal(target_list, ['r', 'a', 'b', ' ', 'o', 'o', 'f'])
         print('Success: test_reverse_inplace')
 
 
 def main():
     test = TestReverse()
-    test.test_reverse()
-    test.test_reverse_inplace()
+    reverse_string = ReverseString()
+    test.test_reverse(reverse_string.reverse)
+    test.test_reverse_inplace(reverse_string.reverse)
 
 
 if __name__ == '__main__':
