b91b6a437b2b64b0f8b04ec2694236b3f5fee726
Donne Martin <donne.martin@gmail.com>
2016-08-14 08:23:11 -0400

    Move bst successor to a class

--- graphs_trees/bst_successor/test_bst_successor.py
+++ graphs_trees/bst_successor/test_bst_successor.py
@@ -6,28 +6,31 @@ class TestBstSuccessor(object):
 
     @raises(Exception)
     def test_bst_successor_empty(self):
-        bst_successor(None)
+        bst_successor = BstSuccessor()
+        bst_successor.get_next(None)
 
     def test_bst_successor(self):
         nodes = {}
         node = Node(5)
         nodes[5] = node
-        nodes[3] = insert(node, 3)
-        nodes[8] = insert(node, 8)
-        nodes[2] = insert(node, 2)
-        nodes[4] = insert(node, 4)
-        nodes[6] = insert(node, 6)
-        nodes[12] = insert(node, 12)
-        nodes[1] = insert(node, 1)
-        nodes[7] = insert(node, 7)
-        nodes[10] = insert(node, 10)
-        nodes[15] = insert(node, 15)
-        nodes[9] = insert(node, 9)
+        bst = Bst(nodes[5])
+        nodes[3] = bst.insert(3)
+        nodes[8] = bst.insert(8)
+        nodes[2] = bst.insert(2)
+        nodes[4] = bst.insert(4)
+        nodes[6] = bst.insert(6)
+        nodes[12] = bst.insert(12)
+        nodes[1] = bst.insert(1)
+        nodes[7] = bst.insert(7)
+        nodes[10] = bst.insert(10)
+        nodes[15] = bst.insert(15)
+        nodes[9] = bst.insert(9)
 
-        assert_equal(bst_successor(nodes[4]), 5)
-        assert_equal(bst_successor(nodes[5]), 6)
-        assert_equal(bst_successor(nodes[8]), 9)
-        assert_equal(bst_successor(nodes[15]), None)
+        bst_successor = BstSuccessor()
+        assert_equal(bst_successor.get_next(nodes[4]), 5)
+        assert_equal(bst_successor.get_next(nodes[5]), 6)
+        assert_equal(bst_successor.get_next(nodes[8]), 9)
+        assert_equal(bst_successor.get_next(nodes[15]), None)
 
         print('Success: test_bst_successor')
 
