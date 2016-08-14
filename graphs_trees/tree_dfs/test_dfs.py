b4ed90e64944b6489a5dda69cec56acc25d5d0e5
Donne Martin <donne.martin@gmail.com>
2016-08-14 08:27:35 -0400

    Move tree dfs to a class

--- graphs_trees/tree_dfs/test_dfs.py
+++ graphs_trees/tree_dfs/test_dfs.py
@@ -7,39 +7,39 @@ class TestDfs(object):
         self.results = Results()
 
     def test_dfs(self):
-        node = Node(5)
-        insert(node, 2)
-        insert(node, 8)
-        insert(node, 1)
-        insert(node, 3)
+        bst = BstDfs(Node(5))
+        bst.insert(2)
+        bst.insert(8)
+        bst.insert(1)
+        bst.insert(3)
 
-        in_order_traversal(node, self.results.add_result)
+        bst.in_order_traversal(bst.root, self.results.add_result)
         assert_equal(str(self.results), "[1, 2, 3, 5, 8]")
         self.results.clear_results()
 
-        pre_order_traversal(node, self.results.add_result)
+        bst.pre_order_traversal(bst.root, self.results.add_result)
         assert_equal(str(self.results), "[5, 2, 1, 3, 8]")
         self.results.clear_results()
 
-        post_order_traversal(node, self.results.add_result)
+        bst.post_order_traversal(bst.root, self.results.add_result)
         assert_equal(str(self.results), "[1, 3, 2, 8, 5]")
         self.results.clear_results()
 
-        node = Node(1)
-        insert(node, 2)
-        insert(node, 3)
-        insert(node, 4)
-        insert(node, 5)
+        bst = BstDfs(Node(1))
+        bst.insert(2)
+        bst.insert(3)
+        bst.insert(4)
+        bst.insert(5)
 
-        in_order_traversal(node, self.results.add_result)
+        bst.in_order_traversal(bst.root, self.results.add_result)
         assert_equal(str(self.results), "[1, 2, 3, 4, 5]")
         self.results.clear_results()
 
-        pre_order_traversal(node, self.results.add_result)
+        bst.pre_order_traversal(bst.root, self.results.add_result)
         assert_equal(str(self.results), "[1, 2, 3, 4, 5]")
         self.results.clear_results()
 
-        post_order_traversal(node, self.results.add_result)
+        bst.post_order_traversal(bst.root, self.results.add_result)
         assert_equal(str(self.results), "[5, 4, 3, 2, 1]")
 
         print('Success: test_dfs')
