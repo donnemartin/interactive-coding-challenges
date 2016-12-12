7b45e27c3a99a90283a0bfc8a3e4777aae9ec7ee
Donne Martin <donne.martin@gmail.com>
2016-12-12 07:21:48 -0500

    Update merge sort challenge
    Update constraints, test cases, code, and tests.

--- sorting_searching/merge_sort/test_merge_sort.py
+++ sorting_searching/merge_sort/test_merge_sort.py
@@ -1,27 +1,23 @@
-from nose.tools import assert_equal
+from nose.tools import assert_equal, assert_raises
 
 
 class TestMergeSort(object):
+
     def test_merge_sort(self):
+        merge_sort = MergeSort()
+
         print('None input')
-        data = None
-        sorted_data = merge_sort(data)
-        assert_equal(sorted_data, None)
+        assert_raises(TypeError, merge_sort.sort, None)
 
         print('Empty input')
-        data = []
-        sorted_data = merge_sort(data)
-        assert_equal(sorted_data, [])
+        assert_equal(merge_sort.sort([]), [])
 
         print('One element')
-        data = [5]
-        sorted_data = merge_sort(data)
-        assert_equal(sorted_data, [5])
+        assert_equal(merge_sort.sort([5]), [5])
 
         print('Two or more elements')
         data = [5, 1, 7, 2, 6, -3, 5, 7, -1]
-        sorted_data = merge_sort(data)
-        assert_equal(sorted_data, sorted(data))
+        assert_equal(merge_sort.sort(data), sorted(data))
 
         print('Success: test_merge_sort')
 
