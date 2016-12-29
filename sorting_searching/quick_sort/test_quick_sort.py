3d49bb1d43f6cd5c81ba99df93304a64588037e7
Donne Martin <donne.martin@gmail.com>
2016-12-29 07:21:56 -0500

    Update quicksort challenge (#134)
    Update constraints, test cases, code, algorithm, and tests.

--- sorting_searching/quick_sort/test_quick_sort.py
+++ sorting_searching/quick_sort/test_quick_sort.py
@@ -1,34 +1,30 @@
-from nose.tools import assert_equal
+from nose.tools import assert_equal, assert_raises
 
 
 class TestQuickSort(object):
-    def test_quick_sort(self, func):
+
+    def test_quick_sort(self):
+        quick_sort = QuickSort()
+
         print('None input')
-        data = None
-        sorted_data = func(data)
-        assert_equal(sorted_data, None)
+        assert_raises(TypeError, quick_sort.sort, None)
 
         print('Empty input')
-        data = []
-        sorted_data = func(data)
-        assert_equal(sorted_data, [])
+        assert_equal(quick_sort.sort([]), [])
 
         print('One element')
-        data = [5]
-        sorted_data = func(data)
-        assert_equal(sorted_data, [5])
+        assert_equal(quick_sort.sort([5]), [5])
 
         print('Two or more elements')
         data = [5, 1, 7, 2, 6, -3, 5, 7, -1]
-        sorted_data = func(data)
-        assert_equal(sorted_data, sorted(data))
+        assert_equal(quick_sort.sort(data), sorted(data))
 
         print('Success: test_quick_sort\n')
 
 
 def main():
     test = TestQuickSort()
-    test.test_quick_sort(quick_sort)
+    test.test_quick_sort()
 
 
 if __name__ == '__main__':
