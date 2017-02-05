cf48629858e7c6bff93d3a205481aaefc620bcda
Donne Martin <donne.martin@gmail.com>
2017-02-04 19:07:06 -0500

    Move xor challenge solution to a class (#143)

--- online_judges/maximizing_xor/test_maximizing_xor.py
+++ online_judges/maximizing_xor/test_maximizing_xor.py
@@ -4,7 +4,8 @@ from nose.tools import assert_equal
 class TestMaximingXor(object):
 
     def test_maximizing_xor(self):
-        assert_equal(max_xor(10, 15), 7)
+        solution = Solution()
+        assert_equal(solution.max_xor(10, 15), 7)
         print('Success: test_maximizing_xor')
 
 
