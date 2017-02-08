2b4397abe0c7d09747aad4ec6256659f02b6159b
Donne Martin <donne.martin@gmail.com>
2017-02-08 06:33:05 -0500

    Rework n pairs parens challenge

--- recursion_dynamic/n_pairs_parentheses/test_n_pairs_parentheses.py
+++ recursion_dynamic/n_pairs_parentheses/test_n_pairs_parentheses.py
@@ -1,24 +1,27 @@
-from nose.tools import assert_equal
+from nose.tools import assert_equal, assert_raises
 
 
 class TestPairParentheses(object):
 
-    def test_pair_parentheses(self, solution):
-        assert_equal(solution(0), set([]))
-        assert_equal(solution(1), set(['()']))
-        assert_equal(solution(2), set(['(())', 
-                                       '()()']))
-        assert_equal(solution(3), set(['((()))', 
-                                       '(()())', 
-                                       '(())()', 
-                                       '()(())', 
-                                       '()()()']))
+    def test_pair_parentheses(self):
+        parentheses = Parentheses()
+        assert_raises(TypeError, parentheses.find_pair, None)
+        assert_raises(ValueError, parentheses.find_pair, -1)
+        assert_equal(parentheses.find_pair(0), [])
+        assert_equal(parentheses.find_pair(1), ['()'])
+        assert_equal(parentheses.find_pair(2), ['(())',
+                                                '()()'])
+        assert_equal(parentheses.find_pair(3), ['((()))',
+                                                '(()())',
+                                                '(())()',
+                                                '()(())',
+                                                '()()()'])
         print('Success: test_pair_parentheses')
 
 
 def main():
     test = TestPairParentheses()
-    test.test_pair_parentheses(pair_parentheses)
+    test.test_pair_parentheses()
 
 
 if __name__ == '__main__':
