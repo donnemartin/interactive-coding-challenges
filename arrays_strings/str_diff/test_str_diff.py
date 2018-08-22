7d53ae70cd298f6fcd03595e06c1dafc058d95e0
Donne Martin <donne.martin@gmail.com>
2018-08-21 23:01:09 -0400

    Update str diff solution
    Update constraints, test cases, and dictionary solution.  Fix inconsistencies between challenge and solution notebooks.

--- arrays_strings/str_diff/test_str_diff.py
+++ arrays_strings/str_diff/test_str_diff.py
@@ -6,8 +6,14 @@ class TestFindDiff(object):
     def test_find_diff(self):
         solution = Solution()
         assert_raises(TypeError, solution.find_diff, None)
+        assert_equal(solution.find_diff('ab', 'aab'), 'a')
+        assert_equal(solution.find_diff('aab', 'ab'), 'a')
         assert_equal(solution.find_diff('abcd', 'abcde'), 'e')
         assert_equal(solution.find_diff('aaabbcdd', 'abdbacade'), 'e')
+        assert_equal(solution.find_diff_xor('ab', 'aab'), 'a')
+        assert_equal(solution.find_diff_xor('aab', 'ab'), 'a')
+        assert_equal(solution.find_diff_xor('abcd', 'abcde'), 'e')
+        assert_equal(solution.find_diff_xor('aaabbcdd', 'abdbacade'), 'e')
         print('Success: test_find_diff')
 
 
