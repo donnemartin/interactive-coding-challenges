5b5b7eaf31985ce5275bd44a538e705194f3d304
monvin <97kilogram@gmail.com>
2019-10-02 16:49:15 +0530

    Update bst.py

--- graphs_trees/bst/bst.py
+++ graphs_trees/bst/bst.py
@@ -1,3 +1,9 @@
+#coding question using bst
+#Binary Search Tree is a node-based binary tree data structure which has the following properties:
+#The left subtree of a node contains only nodes with keys lesser than the node’s key.
+#The right subtree of a node contains only nodes with keys greater than the node’s key.
+#The left and right subtree each must also be a binary search tree.
+
 class Node(object):
 
     def __init__(self, data):
